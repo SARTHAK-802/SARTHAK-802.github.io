694790b0bdbde808b7af27a796d57b2727ec5253
SARTHAK-802 <sarthakmaity450@gmail.com>
2025-12-26 19:23:17 +0000

    Deploying to gh-pages from @ SARTHAK-802/SARTHAK-802.github.io@9bd5e42133cb580e84257fc7c98c08e1472b080a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,6 +16,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
+        },{id: "nav-certificates",
+          title: "certificates",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/certificates/";
+          },
         },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
