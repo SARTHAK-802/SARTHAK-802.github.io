489674e2efb1bcd97366dc2ddece9ead285c2819
SARTHAK-802 <sarthakmaity450@gmail.com>
2025-12-26 18:46:50 +0000

    Deploying to gh-pages from @ SARTHAK-802/SARTHAK-802.github.io@d809c193de085cd94e2c37751cb379547fe45e6b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -374,7 +374,10 @@ ninja.data = [{
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
             },},{id: "news-graduated-with-an-m-sc-in-chemistry-from-iit-indore",
-          title: 'Graduated with an M.Sc. in Chemistry from IIT Indore.',
+          title: 'Graduated with an M.Sc. in Chemistry from IIT Indore!',
+          description: "",
+          section: "News",},{id: "news-i-have-successfully-completed-the-coursera-certified-course-supervised-machine-learning",
+          title: 'I have successfully completed the Coursera-certified course Supervised Machine Learning!',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
